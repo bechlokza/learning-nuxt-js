853e671f04323a7fb1f332222bae9e15a7ea8283
jesper bech løkza <jesper@bechlokza.com>
2022-10-14 13:51:03 +0200

    0202 done

---
+++ learning-nuxt/pages/_slug.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h1>Dynamic Routes</h1>
+    <p>Hey! this is the slug for this page: {{ $route.params.slug }}</p>
+  </div>
+</template>
+
+<script></script>
+
+<style></style>
