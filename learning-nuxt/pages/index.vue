c2205125c7140c20652468dabd29c2508c756cf3
jesper bech løkza <jesper@bechlokza.com>
2022-10-14 15:21:26 +0200

    02_03 async fetch

--- learning-nuxt/pages/index.vue
+++ learning-nuxt/pages/index.vue
@@ -1,9 +1,24 @@
 <template>
-  <p>Index</p>
+  <div>
+    <h1>Most popular rivers in the world</h1>
+    <ul>
+      <li v-for="(river, index) in rivers" :key="`$river-${index}`">
+        {{ river.title }}
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
 export default {
   name: "IndexPage",
+
+  async asyncData({ $http }) {
+    // https://api.nuxtjs.dev/rivers
+    const res = await $http.$get("https://api.nuxtjs.dev/rivers");
+    return {
+      rivers: res,
+    };
+  },
 };
 </script>
